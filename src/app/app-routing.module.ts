7ec5c1c426e5aab22e54936103ceea1dc3c8da95
Santiago Córdova <dokho792@gmail.com>
2021-01-13 18:00:32 -0500

    paginaH

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaginaHComponent } from './pagina-h/pagina-h.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'paginah', component: PaginaHComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
