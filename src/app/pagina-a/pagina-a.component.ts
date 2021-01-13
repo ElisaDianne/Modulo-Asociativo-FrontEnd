9e5b5ad8933a81332e302b1e442543bd8bbb1882
Marcelo Echeverria <marceloecheverria23@gmail.com>
2021-01-13 17:16:01 -0500

    Estructuración del Proyecto.

---
+++ src/app/pagina-a/pagina-a.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-a',
+  templateUrl: './pagina-a.component.html',
+  styleUrls: ['./pagina-a.component.scss']
+})
+export class PaginaAComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
