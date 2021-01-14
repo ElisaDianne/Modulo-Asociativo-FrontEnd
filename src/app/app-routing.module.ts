5eecd8e9044281489bf334e5ce4914b88f32775c
Santiago Córdova <dokho792@gmail.com>
2021-01-14 15:33:05 -0500

    páginaK
    terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PaginaHComponent } from './pagina-h/pagina-h.component';
+import { PaginaKComponent } from './pagina-k/pagina-k.component';
 
-const routes: Routes = [{ path: 'paginah', component: PaginaHComponent }];
+const routes: Routes = [
+  { path: 'paginah', component: PaginaHComponent },
+  { path: 'paginak', component: PaginaKComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
