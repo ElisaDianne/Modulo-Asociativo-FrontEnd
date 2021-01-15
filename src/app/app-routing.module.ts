2e991d343b54db2aa05436c49a07a781a30a7916
MarceloR <mruilova042@puce.edu.ec>
2021-01-14 22:03:42 -0500

    Paginas D y E

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PaginaDComponent } from './pagina-d/pagina-d.component';
+import { PaginaEComponent } from './pagina-e/pagina-e.component';
+
+const routes: Routes = [
+  { path: 'paginad', component: PaginaDComponent },
+  { path: 'paginae', component: PaginaEComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pagina-d/pagina-d.component.ts
+++ src/app/pagina-d/pagina-d.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class PaginaDComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
   }
 
