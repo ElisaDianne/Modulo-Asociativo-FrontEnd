44d0768e2e9af3e14b3ae721f2024956ed9be0c3
Nico2302 <54966721+Nico2302@users.noreply.github.com>
2021-01-16 15:40:11 -0500

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { PaginaAComponent } from './pagina-a/pagina-a.component';
+import { PaginaBComponent } from './pagina-b/pagina-b.component';
+const routes: Routes = [
+  {path: "PaginaA", component: PaginaAComponent },
+  {path: "PaginaB", component: PaginaBComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
