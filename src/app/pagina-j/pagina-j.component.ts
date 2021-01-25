3b1e25ffd1d0f1209b7ea51422669826bc6e6f85
SofiaQuevedo <55284506+SofiaQuevedo@users.noreply.github.com>
2021-01-25 17:28:53 -0500

    cambios

--- src/app/pagina-j/pagina-j.component.ts
+++ src/app/pagina-j/pagina-j.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-pagina-j',
   templateUrl: './pagina-j.component.html',
   styleUrls: ['./pagina-j.component.scss']
 })
 export class PaginaJComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
-  }
+  } 
 
 }
