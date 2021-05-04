2e793f0542bdc8527753a72d565e05315abf0a60
Christian19990124 <71156573+Christian19990124@users.noreply.github.com>
2021-05-04 12:02:30 -0500

    conexion apiA frontend

--- src/app/pagina-a/pagina-a.component.ts
+++ src/app/pagina-a/pagina-a.component.ts
@@ -1,4 +1,11 @@
+import { LOGINComponent } from './../login/login.component';
+import { FormAService } from './../servicios-pag/servicios-pagA/form-a.service';
+import { Md5 } from "ts-md5/dist/md5";
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators,  FormControl } from '@angular/forms';
+import { LoginService } from './../servicios/login.service';
+import { Router } from '@angular/router';
+import { environment } from './../../environments/environment';
 
 @Component({
   selector: 'app-pagina-a',
@@ -6,10 +13,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagina-a.component.scss']
 })
 export class PaginaAComponent implements OnInit {
+  production = environment.production;
+  version = environment.version;
+  version_date = environment.version_date;
+  //formBuilder = any;
+  datosPer = [];
+  mitoken= localStorage.getItem('token');
+  firstConnection = "";
+  statuslogin = false;
+  data_inicial;
+  mestado = 0;
+  formularioDataA : FormGroup = new FormGroup({
+    identi: new FormControl("", [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(
+    public srvApi: FormAService,
+  ) {}
 
   ngOnInit(): void {
+    this.formularioDataA.setValue({
+      identi: '',
+    });
+  }
+  pagAOnline(ruc){
+    console.log(ruc);
+          this.srvApi.getPerson(ruc, this.mitoken).subscribe(
+            (data) => {
+              console.log(data);
+            },
+            (error) => {
+              console.log(error);
+            }
+          )
   }
 
+  async paginaA(form) {
+    var mruc =form.identi;
+
+
+    console.log(mruc);
+    /*const loading = await this.loadingCtrl.create({
+      message: "Cargando...",
+      duration: 1500,
+    })*/
+
+    //await loading.present();
+
+    const ruc = mruc;
+    this.pagAOnline(ruc);
+    //await loading.onDidDismiss();
+
+  }
 }
---
+++ src/app/servicios-pag/servicios-pagA/form-a.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+import {environment} from 'src/environments/environment'
+@Injectable({
+  providedIn: 'root'
+})
+export class FormAService {
+
+  api_token = environment.host + environment.api_token;
+  apiA_persona= environment.apiA_persona;
+  constructor(public http: HttpClient) { }
+
+  /*postLogin(user,pass){
+    var link = this.api_token;
+    const credentiales = environment.credential_app;
+    const head = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization:'Basic ' + credentiales,
+    });
+    let parameters = new URLSearchParams();
+    parameters.set('grant_type','password');
+    parameters.set('username', user);
+    parameters.set('password',pass);
+    parameters.set('idapli','67');
+    return this.http.post(link,parameters.toString(),{headers:head});
+ }*/
+ getPerson(ruc_persona,token){
+  const head =new HttpHeaders({
+    'Content-Type':'application/json',
+    Accept: 'application/json',
+    Authorization:'Bearer ' + token,
+  });
+  var link =
+    this.apiA_persona+
+    + ruc_persona;
+    return this.http.get(link,{headers:head});
+ }
+
+ enviar(datos, token){
+   console.log(datos);
+   const head=new HttpHeaders({
+     "Content-Type": "application/json",
+     Authorization:'Bearer ' + token,
+   });
+   var link=
+     this.api_token + '';
+   return this.http.post(link,datos,{headers:head});
+ }
+}
