bf9217c4283f47159c2e05d9c7af24d2473d96e9
Alberto Mucarsel S <amucarsel@mag.gob.ec>
2021-07-24 16:40:21 -0500

    patchValue added to pagA comp

--- src/app/pagina-a/pagina-a.component.ts
+++ src/app/pagina-a/pagina-a.component.ts
@@ -230,45 +230,43 @@ export class PaginaAComponent implements OnInit {
               //organizacion nueva
               if (res == null) {
                 this.datosPagA = (data);
-              }/*
-                        else if(res.origen ==="BDC" ){
-                        //aqui completamos data
-                          if(res.orgRazonSocial === null){
-                            this.formularioDataA.setValue({
-                             rSocial:data[16]
-                           });
-                          }
-                          if(res.orgActEconomica === null){
-                           this.formularioDataA.setValue({
-                             aEconomica:data[2]
-                           });
-                         }
-
-                         this.formularioDataA.setValue({
-                           nComercial:res.orgNombreComercial,
-                           tFijo:res.orgTelefono,
-                           tCelular:res.orgCelular,
-                           cElectronico:res.orgCorreo,
-                           pWeb:res.orgPaginaWeb,
-                         });
-                        }*/
-
-              else if (res.origen === "Sirus") {
+              }
+              else if (res.origen === "BDC") {
                 //aqui completamos data
                 if (res.organizacionRazonSocial === null) {
-                  this.formularioDataA.setValue({
+                  this.formularioDataA.patchValue({
                     rSocial: data[16]
                   });
                 }
                 if (res.organizacionActividad === null) {
-                  this.formularioDataA.setValue({
+                  this.formularioDataA.patchValue({
                     aEconomica: data[2]
                   });
                 }
-                this.formularioDataA.setValue({
-                  identi:ruc,
-                  rSocial: res.organizacionRazonSocial,
-                  aEconomica: res.organizacionActividad,
+
+                this.formularioDataA.patchValue({
+                  nComercial: res.organizacionNombre,
+                  tFijo: res.organizacionTelefono,
+                  tCelular: res.organizacionCelular,
+                  cElectronico: res.organizacionCorreo,
+                  pWeb: res.orgPaginaWeb,
+                });
+              }
+
+              else if (res.origen === "Sirus") {
+                //aqui completamos data
+                if (res.organizacionRazonSocial === null) {
+                  this.formularioDataA.patchValue({
+                    rSocial: data[16]
+                  });
+                }
+                if (res.organizacionActividad === null) {
+                  this.formularioDataA.patchValue({
+                    aEconomica: data[2]
+                  });
+                }
+                this.formularioDataA.patchValue({
+                  identi: ruc,
                   nComercial: res.organizacionNombre,
                   tFijo: res.organizacionTelefono,
                   tCelular: res.organizacionCelular,
@@ -279,19 +277,17 @@ export class PaginaAComponent implements OnInit {
               else if (res.origen === "BDC_Sirus") {
                 //completamos y comparamos los datos y damos prioridad
                 if (res.organizacionRazonSocial !== data[16]) {
-                  this.formularioDataA.setValue({
+                  this.formularioDataA.patchValue({
                     rSocial: res.organizacionRazonSocial
                   });
                 }
                 if (res.organizacionActividad !== data[2]) {
-                  this.formularioDataA.setValue({
+                  this.formularioDataA.patchValue({
                     aEconomica: res.organizacionActividad
                   });
                 }
                 this.formularioDataA.setValue({
                   identi: ruc,
-                  rSocial: res.organizacionRazonSocial,
-                  aEconomica: res.organizacionActividad,
                   nComercial: res.organizacionNombre,
                   tFijo: res.organizacionTelefono,
                   tCelular: res.organizacionCelular,
@@ -307,7 +303,7 @@ export class PaginaAComponent implements OnInit {
       (error) => {
         console.log("Error inesperado")
         console.log(error);
-        
+
         this.datosPagA = (error);
       }
     )
