8d712ab93d801ddcb2988f98d1fb126de010a743
andresn00 <andreslozada00@hotmail.com>
2021-07-26 18:05:05 -0500

    GIS fixes / UTM

--- src/app/components/gis/gis.component.ts
+++ src/app/components/gis/gis.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { GoogleMapsModule } from '@angular/google-maps';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { GoogleMap, MapMarker, GoogleMapsModule } from '@angular/google-maps';
+
+const utmObj = require('utm-latlng')
+const utm = new utmObj()
 
 @Component({
   selector: 'app-gis',
@@ -7,48 +10,70 @@ import { GoogleMapsModule } from '@angular/google-maps';
   styleUrls: ['./gis.component.scss']
 })
 export class GisComponent implements OnInit {
+  @ViewChild('map') map: GoogleMap
+  marker = {
+    position: {
+      lat: -0.726827638,
+      lng: -79.69816116
+    },
+    label: {
+      color: 'white',
+      text: 'Marcador'
+    },
+    title: 'gmaps',
+    options: {
+      animation: google.maps.Animation.DROP
+    }
+  }
+
   provincia: string
   canton: string
   parroquia: string
   codParroquia: string
 
-  position = {
-    lat: -0.1834458169411667,
-    lng: -78.48399216083874
-  }
-
-  label = {
-    color: 'white',
-    text: 'Marcador'
-  }
-  title = 'gmaps'
-
-  coordenadaX: number = 0
-  coordenadaY: number = 0
-  coordenadaZ: number = 0
-  hemisferio: string = 'aaaaaa'
-  zona: string = 'bbbbbb'
+  xCoordinate: number
+  yCoordinate: number
+  hemisphere: string
+  zoneNumber: number
+  zoneLetter: string
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  getPosition() {
-    return this.position
+  
+  ngAfterViewInit(): void {
+    this.startMap()
   }
-
+  
+  startMap() {
+    this.centerMap(this.marker.position)
+    this.setUtmCoordinates(this.marker.position)    
+  }
   onChooseLocation(e) {
-    console.log(e)
-    const latLng = e.latLng
-    this.position = {
-      lat: latLng.lat(),
-      lng: latLng.lng()
+    const markedPos = {
+      lat: e.latLng.lat(),
+      lng: e.latLng.lng()
     }
-
-    this.coordenadaX = e.Tb.x
-    this.coordenadaY = e.Tb.y
-    this.hemisferio = this.position.lat < 0 ? 'Sur' : 'Norte'
+    this.setMarkerPosition(markedPos)
+    this.centerMap(this.marker.position)
+    this.setUtmCoordinates(markedPos)
   }
 
+  setMarkerPosition(newPos) {
+    this.marker.position = newPos
+  }
+
+  setUtmCoordinates(latLng) {
+    const utmConverted = utm.convertLatLngToUtm(latLng.lat, latLng.lng, 1)
+    this.xCoordinate = utmConverted.Easting
+    this.yCoordinate = utmConverted.Northing
+    this.hemisphere = latLng.lat < 0 ? 'Sur' : 'Norte'
+    this.zoneNumber = utmConverted.ZoneNumber
+    this.zoneLetter = utmConverted.ZoneLetter
+  }
+
+  centerMap(pos) {
+    this.map.panTo(pos)
+  }
 }
